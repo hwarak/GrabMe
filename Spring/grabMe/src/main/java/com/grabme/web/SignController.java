1d40a1c837f5c616559fb3807764c2415a9d1ace
hwarak <hwarakhada@gmail.com>
2021-02-23 19:52:34 +0900

    update controller

--- Spring/grabMe/src/main/java/com/grabme/web/SignController.java
+++ Spring/grabMe/src/main/java/com/grabme/web/SignController.java
@@ -1,20 +1,15 @@
 package com.grabme.web;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grabme.service.MessageService;
 import com.grabme.service.UserService;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api("SignController")
@@ -33,6 +28,8 @@ public class SignController {
 		//message_service.sendMessage(number);
 
 		//return "{\"result\":\"ok\"}";
+		
+
 		return "{\"result\":\""+number+"}";
 
 	}
