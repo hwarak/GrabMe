5a3e2ae741d1a4ba5123fdd2d7c7e3dd4991bf8a
hwarak <hwarakhada@gmail.com>
2021-02-23 14:18:37 +0900

    update user/shop dao,service

--- Spring/grabMe/src/main/java/com/grabme/dao/ShopDAOImpl.java
+++ Spring/grabMe/src/main/java/com/grabme/dao/ShopDAOImpl.java
@@ -1,5 +1,29 @@
 package com.grabme.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ShopDAOImpl implements ShopDAO {
+
+	@Autowired
+	SqlSession sqlsession;
+
+	@Override
+	public void insertShop(int user_idx, int category_idx, String thumbnail, String title, String address,
+			String introduction) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("user_idx", user_idx);
+		map.put("category_idx", category_idx);
+		map.put("thumbnail", thumbnail);
+		map.put("title", title);
+		map.put("address", address);
+		map.put("introduction", introduction);
+
+		sqlsession.insert("com.grabme.mappers.ShopMapper.insertShop", map);
+
+	}
 
 }
--- Spring/grabMe/src/main/java/com/grabme/service/ShopServiceImpl.java
+++ Spring/grabMe/src/main/java/com/grabme/service/ShopServiceImpl.java
@@ -1,5 +1,22 @@
 package com.grabme.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.grabme.dao.ShopDAO;
+
+@Service
 public class ShopServiceImpl implements ShopService {
+	
+	@Autowired
+	ShopDAO dao;
+
+	@Override
+	public void insertShop(int user_idx, int category_idx, String thumbnail, String title, String address,
+			String introduction) {
+		dao.insertShop(user_idx, category_idx, thumbnail, title, address, introduction);
+	}
+	
+	
 
 }
