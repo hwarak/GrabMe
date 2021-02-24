2d977575732fa7f9027ff5af6c4668c9b35afaa3
hwarak <hwarakhada@gmail.com>
2021-02-24 16:38:58 +0900

    update Test File

--- Spring/grabMe/src/test/java/com/grabme/web/UserTest.java
+++ Spring/grabMe/src/test/java/com/grabme/web/UserTest.java
@@ -1,13 +1,27 @@
 package com.grabme.web;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.grabme.dao.UserDAO;
+
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/root-context.xml" })
 public class UserTest {
-	
-	
+
+	@Autowired
+	private UserDAO dao;
+
+	@Test
+	public void testCheckPhone() throws Exception {
+		// Check phone in database
+		System.out.println(dao.checkPhone("01023652365"));
+
+	}
 
 }
