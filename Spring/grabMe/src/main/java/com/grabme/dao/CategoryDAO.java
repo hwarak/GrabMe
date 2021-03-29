5514236f12828a54add898082005e96e42a059c2
hwarak <hwarakhada@gmail.com>
2021-03-29 16:27:41 +0900

    feat: Calculate and sort distances by hardness latitude value

--- Spring/grabMe/src/main/java/com/grabme/dao/CategoryDAO.java
+++ Spring/grabMe/src/main/java/com/grabme/dao/CategoryDAO.java
@@ -6,7 +6,7 @@ import com.grabme.vo.ShopVO;
 
 public interface CategoryDAO {
 
-	// select Category With X,Y
-	public List<ShopVO> selectCategoryWithXY(double x, double y, int category_idx);
+	// 위도,경도 값 받아서 거리순으로 정렬후 원하는 만큼 보여주기
+	public List<ShopVO> selectCategoryWithXY(double x, double y, int category_idx, int startNum, int endNum);
 	
 }
